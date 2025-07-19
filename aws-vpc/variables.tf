db53489f9d8b73d8c132346ece1edfd9ae68c878
sri <you@gmail.com>
2025-07-19 14:46:56 +0530

    new project

---
+++ aws-vpc/variables.tf
@@ -0,0 +1,20 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDRs"
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of private subnet CIDRs"
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
